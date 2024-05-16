94303319409c1e09d7f13457e97da1d79a781008
Franklin-Gomez <franklin_043097@hotmail.com>
2024-05-16 09:33:16 -0500

    working on cart-reducer.ts

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -1,5 +1,7 @@
+import { act } from "react-dom/test-utils"
 import { db } from "../db/db"
 
+// actions
 export type CartActions = 
 { type : 'addToCart' , payload : { item : Guitar }} |
 { type : 'removeFromCart' , payload : { id : Guitar['id'] }} |
@@ -7,12 +9,62 @@ export type CartActions =
 { type : 'decrementQuantity' , payload : { id : Guitar['id'] }} |
 { type : 'cleanCart'} 
 
+// types 
 export type CartState = { 
     data : Guitar[],
     cart : cartItem[]
 }
 
+// states
 export const  initialState : CartState = {
     data : db,
     cart : []
 }
+
+
+// Reducer
+export const cartReducer = ( 
+
+    state : CartState = initialState,
+    actions : CartActions
+
+    ) => { 
+
+    // dispatch 
+    if( actions.type == "addToCart") { 
+        return {
+            ...state
+        }
+    }
+
+    if( actions.type == "removeFromCart") { 
+        return {
+            ...state
+        }
+    }
+
+    if( actions.type == "increaseyQuantity") { 
+        return {
+            ...state
+        }
+    }
+
+    if( actions.type == "decrementQuantity") { 
+        return {
+            ...state
+        }
+    }
+
+    if( actions.type == "cleanCart") { 
+        return {
+            ...state
+        }
+    }
+
+    return state
+
+}
+
+
+
+
